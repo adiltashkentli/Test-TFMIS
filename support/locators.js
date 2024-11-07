0cf60876994eb4072ff27f06dc47f31e2ceb3a46
Odil Tashkentli <odiltashkentu@gmail.com>
2024-11-07 15:40:10 +0500

    Budget Preparation

--- support/locators.js
+++ support/locators.js
@@ -3,9 +3,15 @@ MainPage: {
     headText: '//*[@id="root"]/div[2]/div[2]/div/div/div[2]/span[1]',
     headTextSec: '#root > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(2) > span:nth-child(2)',
     nationalArm: '#root > div:nth-child(2) > div:nth-child(2) > div > div > div:nth-child(1) > img',
-    loginInput: 'id=loginForm_login',
+    
+},
+Dashboard: {
+    login: 'id=loginForm_login',
     password: 'id=loginForm_password',
-    loginButton: '#loginForm > div:nth-child(2) > button'
+    loginButton: '#loginForm > div:nth-child(2) > button',
+    headings: '//*[@id="root"]/div[2]/header/div/div[2]/div/a',
+    budgPrpMenusList: '//ul[@id="rc-menu-uuid-81808-1-2-popup"]//li/div/span',
+    langSlcBtn: '//*[@id="root"]/div[2]/header/div/div[2]/div/div/div/span[2]/div/span',
 }
 }
 module.exports = Locators;
