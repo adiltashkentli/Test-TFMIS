14d9dd37949b06ab0750ac708855cd4f6be0a4f4
Odil Tashkentli <odiltashkentu@gmail.com>
2024-11-08 17:56:39 +0500

    Revenue Ceiling 1 v

--- support/locators.js
+++ support/locators.js
@@ -12,6 +12,19 @@ Dashboard: {
     headings: '//*[@id="root"]/div[2]/header/div/div[2]/div/a',
     budgPrpMenusList: '//ul[@id="rc-menu-uuid-81808-1-2-popup"]//li/div/span',
     langSlcBtn: '//*[@id="root"]/div[2]/header/div/div[2]/div/div/div/span[2]/div/span',
-}
+},
+Revenue_ceiling: {
+    categoryTabName: '//*[@id="root"]/div[2]/div/div/div[3]/div[1]/div/div/p', //for assertion
+    lockButton: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[1]/div/div[1]/div',  //for clikcable assertion
+    inputAge: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div/svg',
+    ageSelectList: '//*[@id=":ro:"]//li',
+    inputScenario: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div[2]/div/div/svg',
+    scenarioSelectList: '//*[@id=":rq:"]//li',
+    listButton: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div[3]/div[1]/div',
+    colunmHeadersContainer: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div',//for assertion
+    reportButton: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div[1]', //for clikcable assertion
+    saveButton: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div[2]',//for clikcable assertion
+    paginationButton: '//*[@id=":rr:"]',
+},
 }
 module.exports = Locators;
