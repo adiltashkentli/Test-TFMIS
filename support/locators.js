5ffb0cc8468333b45dfc62ea81b53ed88b5e5c02
Odil Tashkentli <odiltashkentu@gmail.com>
2024-11-14 13:39:27 +0500

    Income by districts

--- support/locators.js
+++ support/locators.js
@@ -14,12 +14,13 @@ Dashboard: {
     langSlcBtn: '//*[@id="root"]/div[2]/header/div/div[2]/div/div/div/span[2]/div/span',
 },
 IncomeCeiling: {
-    budPrepMenuBut: '//*[@id="root"]/div[2]/div/div/div[1]/div/div/ul/li[2]/div/span[2]',
+    budPrepMenuBut: 'li:nth-child(2) div:nth-child(1) span:nth-child(2)',
+    menuRevenue: '//span[contains(text(),"Доходы")]',
     incomeCeilingButton: '[data-menu-id="specific-menu-id"] > li:nth-child(2)',
     categoryTabName: '//*[@id="root"]/div[2]/div/div/div[3]/div[1]/div/div/p', //for assertion
     lockButton: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[1]/div/div[1]/div/img',  //for clikcable assertion
-    selectAge: '//*[@id="demo-simple-select"]',
-    selectScenario: '//*[@id="demo-simple-select"]',
+    selectAge: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[2]/div/div[1]/div/div',
+    selectScenario: '(//div[@id="demo-simple-select"])[2]',
     secondItemInScenario: '#\\:rq\\: > li:nth-child(3)',    
     colunmHeadersContainer: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[3]/div/div[1]/div[2]/div[1]/div/div/div',//for assertion
     reportButton: '//*[@id="root"]/div[2]/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div[1]', //for clikcable assertion    
@@ -34,5 +35,13 @@ IncomeByRegions: {
     pagination: '10',
     selectPagination: '#\:rd5\: > li:nth-child(2)',
 },
+IncomeByDistricts: {
+    menuDistrictRevenue: '//p[@title="income_by_districts"]',
+    tabHeading: '//*[@id="root"]/div[2]/div/div/div[3]/div[1]/div/div/p',
+    selectAge: '(//div[@id="demo-simple-select"])[1]',
+    listButton: 'div[class="IncomeRegion_SelectRegion__DQsPf"] p',
+
+},
+
 }
 module.exports = Locators;
