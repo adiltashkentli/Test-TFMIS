5098ee1cd7cf2449caf9e20e6fd614699d120e6e
Odil Tashkentli <odiltashkentu@gmail.com>
2024-11-15 18:33:45 +0500

    Income Application Registry

---
+++ pages/IncomeApplicationRegistry.js
@@ -0,0 +1,103 @@
+const { expect } = require('@playwright/test');
+const Locators = require('../support/locators');
+const dataUser = require('../data/dataUser');
+const exp = require('constants');
+
+class IncomeApplicationRegistry {
+    constructor(page) {
+        this.page = page;
+    }
+
+    async navigateToPage() {
+        await this.page.click(Locators.IncomeCeiling.budPrepMenuBut);
+        await this.page.click(Locators.IncomeCeiling.menuRevenue);
+        await this.page.click(Locators.IncomeApplicationRegistry.menuIncomeApplicationRegistry);
+    }
+
+    async checkTabHeader() {
+        const tabHeader = await this.page.locator(Locators.IncomeApplicationRegistry.tabHeader);
+        await expect(tabHeader).toContainText('Реестр доходных заявок');
+    }
+
+    async selectElementsToList() {
+        await this.page.getByLabel('Год').first().click();
+        await this.page.getByRole('option', { name: 'Ten' }).click();
+        await this.page.getByLabel('Год').nth(1).click();
+        await this.page.getByRole('option', { name: 'Twenty' }).click();
+        await this.page.getByLabel('Год').nth(2).click();
+        await this.page.getByRole('option', { name: 'Thirty' }).click();
+        await this.page.getByLabel('Год').nth(3).click();
+        await this.page.getByRole('option', { name: 'Ten' }).click();
+        await this.page.getByLabel('Год').nth(4).click();
+        await this.page.getByRole('option', { name: 'Twenty' }).click();
+        await this.page.getByText('Список').click();
+    }
+
+    async pagination() {
+        await this.page.getByLabel(Locators.IncomeByRegions.pagination).click();
+        await this.page.getByRole('option', { name: '30' }).click();
+    }
+    async listOfSpreadsheet() {
+        // Get all headers of spreadsheet
+        const tableHeadings = await this.page.$$(Locators.IncomeApplicationRegistry.spreadsheetHeaders);
+        const expectedTexts = [
+            '№',
+            'Администратор',
+            'Наименование администратора',
+            'Терр.код',
+            'Наименование территории',
+            'ИФ',            
+            'Наименование ИФ',
+            'Статус',
+            'Операции',
+        ];
+
+        for (let i = 0; i < tableHeadings.length; i++) {
+            const headersText = await tableHeadings[i].textContent();
+            console.log(headersText);
+
+            // Assert that the menu text matches the expected value
+            expect(headersText.trim()).toBe(expectedTexts[i]);
+        }
+    }
+    async ckeckAllBoxesRadioButton(){
+        //check and assert
+        const checkAllBoxes = this.page.locator(Locators.IncomeApplicationRegistry.checkAllBoxesRadioButton);
+        await checkAllBoxes.click();
+        await expect(checkAllBoxes).toHaveAttribute('aria-checked', 'true');
+        //uncheck and assert
+        const uncheckAllBox =  this.page.locator(Locators.IncomeApplicationRegistry.checkAllBoxesRadioButton);
+        await uncheckAllBox.click();
+        await expect(uncheckAllBox).not.toBeChecked();
+    }
+    async checkOneLineBox(){
+        const checkOneBox = this.page.locator(Locators.IncomeApplicationRegistry.checkOneBox);
+        await checkOneBox.click();
+        await expect(checkOneBox).toHaveAttribute('aria-checked', 'true');
+        const uncheckOneBox = this.page.locator(Locators.IncomeApplicationRegistry.checkOneBox);
+        await uncheckOneBox.click();
+        await expect(uncheckOneBox).not.toBeChecked();
+    }
+    async allowSignButtonAssertion() {
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.allowSignButton)).toBeVisible();
+    }
+    async approveButtonAssertion() {
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.approveButton)).toBeVisible();
+    }
+    async endorseButtonAssertion(){
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.endorseButton)).toBeVisible();
+    }
+    async agreedButtonAssertion(){
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.agreedButton)).toBeVisible();
+    }
+    async refuseApproveButtonAssertion(){
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.refuseToApproveButton)).toBeVisible();
+    }
+    async denyToApproveButtonAssertion(){
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.denyToApproveButton)).toBeVisible();
+    }
+    async refuseToReconcileButtonAssertion(){
+        await expect(this.page.locator(Locators.IncomeApplicationRegistry.refuseToReconcileButton)).toBeVisible();
+    }
+}
+module.exports = IncomeApplicationRegistry;
---
+++ tests/Incomes_Dohodi/incomeApplicationRegistry.spec.js
@@ -0,0 +1,57 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const IncomeApplicationRegistry = require('../../pages/IncomeApplicationRegistry')
+const Dashboard = require('../../pages/Dashboard');
+const { assert } = require('console');
+
+test.describe('Реестр доходных заявок', () => {
+  let incomeApplicationRegistry;
+  let dashboard;
+  
+  test.beforeEach(async ({page}) => {
+    incomeApplicationRegistry = new IncomeApplicationRegistry(page);
+    dashboard = new Dashboard(page);
+    await dashboard.login();
+    await incomeApplicationRegistry.navigateToPage();
+  });
+
+  test ('Check tab header', async ()=> {
+    await incomeApplicationRegistry.checkTabHeader();
+  });
+  test ('Select elements to relarive list', async()=>{
+    await incomeApplicationRegistry.selectElementsToList();
+  });
+  test ('Increase pagination', async()=>{
+    await incomeApplicationRegistry.pagination();
+  });
+  test ('Get spreadsheet headers', async()=>{
+    await incomeApplicationRegistry.listOfSpreadsheet();
+  });
+  test ('Check all boxes radio button', async()=>{
+    await incomeApplicationRegistry.ckeckAllBoxesRadioButton();
+  });
+  test ('Check one box radio button', async()=>{
+    await incomeApplicationRegistry.checkOneLineBox();
+  })
+  test ('Check allow to sign button', async()=>{
+    await incomeApplicationRegistry.allowSignButtonAssertion();
+  });
+  test ('Check approve button', async()=>{
+    await incomeApplicationRegistry.approveButtonAssertion();
+  });
+  test ('Check endorse button', async()=>{
+    await incomeApplicationRegistry.endorseButtonAssertion();
+  });
+  test ('Check agreed button', async()=>{
+    await incomeApplicationRegistry.agreedButtonAssertion();
+  });
+  test ('Check refuse to approve button', async()=>{
+    await incomeApplicationRegistry.refuseApproveButtonAssertion();
+  });
+  test ('Check deny to approve button', async()=>{
+    await incomeApplicationRegistry.denyToApproveButtonAssertion();
+  });
+  test ('Check refuse to reconcile button', async()=>{
+    await incomeApplicationRegistry.refuseToReconcileButtonAssertion();
+  });
+})
