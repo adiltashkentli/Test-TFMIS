ee3500865b6f3216c3eb871d4efac535577957ed
Odil Tashkentli <odiltashkentu@gmail.com>
2024-11-21 09:51:34 +0500

    Category: Registry of Costs Requests

--- tests/Outcomes_Rashodi/registryOfCostsRequests.spec.js
+++ tests/Outcomes_Rashodi/registryOfCostsRequests.spec.js
@@ -1,35 +1,41 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-const BudgetRequests = require('../../pages/BudgetRequests')
+const RegistryOfCostsRequests = require('../../pages/RegistryOfCostsRequests')
 const Dashboard = require('../../pages/Dashboard');
 const { assert } = require('console');
 const IncomeByRegions = require('../../pages/IncomeByRegions');
 
 test.describe('Категория: Бюджетные заявки "ПБС"', () => {
-  let budgetRequests;
+  let registryOfCostsRequests;
   let dashboard;
   let incomeByRegions;
   
   test.beforeEach(async ({page}) => {
-    budgetRequests = new BudgetRequests(page);
+    registryOfCostsRequests = new RegistryOfCostsRequests(page);
     dashboard = new Dashboard(page);
     incomeByRegions = new IncomeByRegions(page);
     await dashboard.login();
-    await budgetRequests.navigateToPage();
+    await registryOfCostsRequests.navigateToPage();
   });
   test ('Check tab header', async ()=> {
-    await budgetRequests.checkTabHeader();
-  });  
-  test ('Check pagination', async()=>{
-    await budgetRequests.pagination();
-  }); 
+    await registryOfCostsRequests.checkTabHeader();
+  });   
   test ('Get relative list', async()=>{
-    await budgetRequests.selectElementsToList();
+    await registryOfCostsRequests.selectElementsToList();
   });  
   test ('Get spreadsheet headings', async()=>{
-    await budgetRequests.listOfSpreadsheet();
+    await registryOfCostsRequests.listOfSpreadsheet();
   });
-  test ('Check report button', async()=>{
-    await budgetRequests.reportButtonAssertion();
+  test ('Check allow to sign button', async()=>{
+    await registryOfCostsRequests.allowSignButtonAssertion();
   });
+  test ('Check approve button', async()=>{
+    await registryOfCostsRequests.approveButtonAssertion();
+  });
+  test ('Check endorse button', async()=>{
+    await registryOfCostsRequests.endorseButtonAssertion();
+  });
+  test ('Check agreed button', async()=>{
+    await registryOfCostsRequests.agreedButtonAssertion();
+  })
 })
