cd444da184aa72389ff2fc5e0c1d407a91ec84b8
Odil Tashkentli <odiltashkentu@gmail.com>
2024-12-02 16:11:40 +0500

    Category: Salary Calculation Registry

--- support/locators.js
+++ support/locators.js
@@ -340,9 +340,12 @@ ContractsRegistry: {
 },
 ConractObligationsReg: {
     categoryMenu: "(//p[@title='contract_obligations_registry'])[1]",
-    spreadsheetHeaders: '//*[@id="root"]//div[2]/div/div/div[2]/div/div[1]/div[2]/div[1]/div/div/div',
+    spreadsheetHeaders: '//*[@id="root"]//div[2]/div/div/div[2]/div/div[1]/div[2]/div[1]/div/div/div',    
 },
-
+SalaryCalcReg: {
+    categoryMenu: "(//p[@title='salary_calculations_registry'])[1]",
+    spreadsheetHeaders: '//*[@id="root"]//div[2]/div/div/div[2]/div[1]/div[1]/div[2]/div[1]/div/div/div'
+},
 
 }
 
