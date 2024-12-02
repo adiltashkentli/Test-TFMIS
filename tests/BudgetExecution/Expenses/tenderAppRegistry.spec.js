2340dfb530ef57d4f33a5a79e9010ea720bf07f6
Odil Tashkentli <odiltashkentu@gmail.com>
2024-12-02 14:55:40 +0500

    Category: Tender App Registry

---
+++ tests/BudgetExecution/Expenses/tenderAppRegistry.spec.js
@@ -0,0 +1,32 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const TenderAppReg = require ('../../../pages/BudExecTenderAppRegistry');
+const Dashboard = require('../../../pages/Dashboard');
+const { assert } = require('console');
+
+test.describe('Категория: Реестр тендерных заявок', () => {
+  let tenderAppReg;
+  let dashboard;
+    
+  test.beforeEach(async ({page}) => {
+    tenderAppReg = new TenderAppReg(page);
+    dashboard = new Dashboard(page);    
+    await dashboard.login();
+    await tenderAppReg.navigateToPage();
+  });
+  test ('Check tab header', async()=>{
+    await tenderAppReg.checkTabHeader();
+  });
+  test ('Select relative data get list', async()=>{
+    await tenderAppReg.chooseRelativeDataToList();
+  });
+  test ('Get spreadsheet headings', async()=>{
+    await tenderAppReg.checkSpreadsheetHeadings();
+  });
+  test ('Increase pagination', async()=>{
+    await tenderAppReg.pagination();
+  });
+  test ('Check footer areas', async()=>{
+    await tenderAppReg.footerAreas();
+  });
+})
