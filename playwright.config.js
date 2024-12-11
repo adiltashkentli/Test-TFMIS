0fa13ebe3a2c1e986e61368ca85d2d463c8b465d
Odil Tashkentli <odiltashkentu@gmail.com>
2024-12-11 10:56:36 +0500

    Category: Salary (Recipients)

--- playwright.config.js
+++ playwright.config.js
@@ -39,7 +39,7 @@ module.exports = defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://tfmis.vercel.app/',
     actionTimeout: 30000, // Timeout for actions
-    slowMo: 3000,
+    slowMo: 5000,
     navigationTimeout: 450000,  // 30 seconds timeout for page loads (optional)
     waitUntil: 'load',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
